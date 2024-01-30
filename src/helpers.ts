d36cbaa7a79e54f1f2d5013b03f1e8f24bb66187
ap🌈 <ap.phitsanu@gmail.com>
2024-01-30 20:46:54 +0700

    feat: migrate AWS-SDK to V3

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,15 +5,7 @@ export const trimLastSlash = (str: string | undefined): string => {
     return '';
   }
 
-  return str.substring(str.length - 1) === '/'
-    ? str.substring(0, str.length - 1)
-    : str;
-};
-
-export const getFileName = (file: UploadedFile): string => {
-  const index = file.originalname.lastIndexOf('.');
-
-  return file.originalname.slice(0, index);
+  return str.endsWith('/') ? str.substring(0, str.length - 1) : str;
 };
 
 export const getFileExtension = (file: UploadedFile): string => {
@@ -22,34 +14,10 @@ export const getFileExtension = (file: UploadedFile): string => {
   return file.originalname.slice(index + 1);
 };
 
-export const hasExtension = (str: string | undefined): boolean => {
-  if (typeof str === 'undefined') {
+export const hasExtension = (str: string): boolean => {
+  if (str.length === 0) {
     return false;
   }
 
-  return str.lastIndexOf('.') > 0 ? true : false;
-};
-
-export const getFileNameFromStr = (str: string | undefined): string => {
-  if (typeof str === 'undefined') {
-    return '';
-  }
-
-  if (!hasExtension(str)) {
-    return str;
-  }
-
-  return str.slice(0, str.lastIndexOf('.'));
-};
-
-export const getFileExtensionFromStr = (str: string | undefined): string => {
-  if (typeof str === 'undefined') {
-    return '';
-  }
-
-  if (!hasExtension(str)) {
-    return '';
-  }
-
-  return str.slice(str.lastIndexOf('.') + 1);
+  return str.lastIndexOf('.') > 0;
 };
